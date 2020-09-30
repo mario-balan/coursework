e03d0c4a44c461a2b91bcaf6bef10090b02d75b0
Mario Balan <mario.balan@gmail.com>
2020-09-29 23:55:42 -0300

    pythonds: update exercise on list generation.

--- runestone/pythonds/complexity_generating_lists.py
+++ runestone/pythonds/complexity_generating_lists.py
@@ -20,7 +20,14 @@ def listComprehension():
 def listRange():
     l = list(range(1000))
 
+def empty():
+    pass
 
+
+# the imports below are intended to execute function in the timeit environment.
+# the times reported include the time spent in function calling.
+
+print("Timings including function calls:")
 t1 = Timer("concatenate()", "from __main__ import concatenate")
 print("Concatenate: ",t1.timeit(number=1000), "milliseconds.")
 t2 = Timer("append()", "from __main__ import append")
@@ -29,3 +36,18 @@ t3 = Timer("listComprehension()", "from __main__ import listComprehension")
 print("listComprehension: ",t3.timeit(number=1000), "milliseconds.")
 t4 = Timer("listRange()", "from __main__ import listRange")
 print("listRange: ",t4.timeit(number=1000), "milliseconds.")
+
+print("\nTimings excluding function calls:")
+t5 = Timer("empty()", "from __main__ import empty")
+empty = t5.timeit(number=1000)
+
+t1 = Timer("concatenate()", "from __main__ import concatenate")
+print("Concatenate: ",t1.timeit(number=1000) - empty, "milliseconds.")
+t2 = Timer("append()", "from __main__ import append")
+print("Append: ",t2.timeit(number=1000) - empty, "milliseconds.")
+t3 = Timer("listComprehension()", "from __main__ import listComprehension")
+print("listComprehension: ",t3.timeit(number=1000) - empty, "milliseconds.")
+t4 = Timer("listRange()", "from __main__ import listRange")
+print("listRange: ",t4.timeit(number=1000) - empty, "milliseconds.")
+
+print("\nreference (time to run an empty function): ",empty, "milliseconds.")
