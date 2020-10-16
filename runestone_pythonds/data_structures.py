c58f4bd23b5bd3b90cd15955a55d37e32ebb643d
Mario Balan <mario.balan@gmail.com>
2020-10-16 15:01:19 -0300

    pythonds: add Queue to data structures

--- runestone_pythonds/data_structures.py
+++ runestone_pythonds/data_structures.py
@@ -18,3 +18,22 @@ class Stack:
 
      def size(self):
          return len(self.items)
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
