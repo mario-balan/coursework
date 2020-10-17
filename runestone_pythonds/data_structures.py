df2df5e4f9f4a737977e602968e1d60474c5b3ec
Mario Balan <mario.balan@gmail.com>
2020-10-17 18:25:21 -0300

    pythonds: add Deque data structure

--- runestone_pythonds/data_structures.py
+++ runestone_pythonds/data_structures.py
@@ -37,3 +37,31 @@ class Queue:
 
     def size(self):
         return len(self.items)
+
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def addFront(self, item):
+        self.items.append(item)
+
+    def addRear(self, item):
+        self.items.insert(0,item)
+
+    def peekFront(self):
+        return self.items[len(self.items)-1]
+
+    def peekRear(self):
+        return self.items[0]
+
+    def removeFront(self):
+        return self.items.pop()
+
+    def removeRear(self):
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
