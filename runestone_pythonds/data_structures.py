875888cb1d9392512cee2027081d5eb05d5844e1
Mario Balan <mario.balan@gmail.com>
2020-10-20 23:41:09 -0300

    pythonds: add append function to unordered list

--- runestone_pythonds/data_structures.py
+++ runestone_pythonds/data_structures.py
@@ -114,17 +114,42 @@ class UnorderedList:
                 current = current.getNext()
         return found
 
-def remove(self,item):
-    current = self.head
-    previous = None
-    found = False
-    while not found:
-        if current.getData() == item:
-            found = True
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+        if previous == None:
+            self.head = current.getNext()
         else:
+            previous.setNext(current.getNext())
+
+    def append(self,item):
+        temp = Node(item)
+        current = self.head
+        previous = None
+
+        while current != None:
             previous = current
             current = current.getNext()
-    if previous == None:
-        self.head = current.getNext()
-    else:
-        previous.setNext(current.getNext())
+
+        previous.setNext(temp)
+
+mylist = UnorderedList()
+
+mylist.add(31)
+mylist.add(77)
+mylist.add(17)
+mylist.add(93)
+mylist.add(26)
+mylist.add(54)
+print(mylist.size())
+print(mylist.search(26))
+print(mylist.search(22))
+mylist.append(22)
+print(mylist.search(22))
